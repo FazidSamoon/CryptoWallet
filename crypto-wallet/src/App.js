b4537ce00ee8cc18cfd52a448185b1179dae11b6
Fazid Samoon <tuan.fazid@outlook.com>
2022-07-04 00:52:22 +0530

    Navbar created

--- crypto-wallet/src/App.js
+++ crypto-wallet/src/App.js
@@ -1,9 +1,41 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  News,
+  CryptoDetails,
+  Cryptocurrencies,
+  Exchanges,
+  HomePage,
+} from "./components";
 
 function App() {
-  return <div className="App">
-    <h1>crypto wallettt</h1>
-  </div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />}></Route>
+
+              <Route path="/exchanges" element={<Exchanges />}></Route>
+
+              <Route path="/cryptocurrency" element={<Cryptocurrencies />}></Route>
+
+              <Route path="/crypto/:coi" element={<CryptoDetails />}></Route>
+
+              <Route path="/news" element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
 }
 
 export default App;
---
+++ crypto-wallet/src/components/Exchanges.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Exchanges() {
+  return (
+    <div>exchanges</div>
+  )
+}
+
+export default Exchanges
